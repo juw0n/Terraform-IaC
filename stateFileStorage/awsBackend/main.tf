2fe841ca0884c960685e4a9ea4656e7743b6ca11
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-11-07 22:35:13 +0100

    define bucket

--- stateFileStorage/awsBackend/main.tf
+++ stateFileStorage/awsBackend/main.tf
@@ -13,3 +13,27 @@ provider "aws" {
     shared_credentials_files = ["~/.aws/credentials"]
     profile                  = "vscodeTerraform"
 }
+
+resource "aws_s3_bucket" "terraform_state_s3" {
+  bucket        = "terraform-state"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket        = aws_s3_bucket.terraform_state_s3.bucket 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
