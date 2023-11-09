80b9a3c4f546737ac549f60b903da0382a1e38a1
Oluwajuwon ODUNITAN <oduns07@gmail.com>
2023-11-09 16:10:32 +0100

    modify the gitignore

--- stateFileStorage/awsBackend/main.tf
+++ stateFileStorage/awsBackend/main.tf
@@ -1,34 +1,50 @@
+# configuring terraform provider -- run this first before creating the cloud backend
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+# config for aws backend for statefile
 terraform {
-    required_providers {
-       aws = {
-        source = "hashicorp/aws"
+  backend "s3" {
+    bucket         = "juwon_terraform_statefile"
+    dynamodb_table = "terraform-state-locking"
+    key            = "tf/statefile/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
       version = "~> 5.0"
-       }
     }
+  }
 }
 # Configure the AWS Provider
 provider "aws" {
-    region                   = "us-east-1"
-    shared_config_files      = ["~/.aws/config"]
-    shared_credentials_files = ["~/.aws/credentials"]
-    profile                  = "vscodeTerraform"
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscodeTerraform"
 }
-
-resource "aws_s3_bucket" "terraform_state_s3" {
-  bucket        = "terraform-state"
+# create an s3 bucket too store the statefile
+resource "aws_s3_bucket" "juwon_terraform_statefile" {
+  bucket        = "juwon-tf-bucket"
   force_destroy = true
 }
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket        = aws_s3_bucket.terraform_state_s3.bucket 
+# apply server side AES256 encrption
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_config" {
+  bucket = aws_s3_bucket.juwon_terraform_statefile.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
+# create dynamobd to lock the state file with using terraform apply
+resource "aws_dynamodb_table" "juw0n_terraform_locks" {
   name         = "terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
